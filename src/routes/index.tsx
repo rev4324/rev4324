c3ef9db3d5c0b65e0e5317dcaa8245f9722765b0
rev4324 <fevenant13@gmail.com>
2024-03-30 14:14:48 +0100

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,29 @@
+import { Title } from "@solidjs/meta";
+import "./index.css";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>rev4324</Title>
+      <div role="presentation" class="bar"></div>
+
+      <div>
+        <h1>rev4324</h1>
+
+        <ul class="list">
+          <li>
+            <a class="link">GitHub</a>
+          </li>
+
+          <li>
+            <a class="link">X.com (formerly Twitter)</a>
+          </li>
+
+          <li>
+            <a class="link">Telegram</a>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
