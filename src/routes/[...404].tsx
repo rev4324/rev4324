3ac45ae42bc4a2b7665d424947e6e2d53dcaf320
rev4324 <fevenant13@gmail.com>
2024-04-10 21:06:10 +0200

    feat: improve 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,16 +4,14 @@ import { HttpStatusCode } from "@solidjs/start";
 export default function NotFound() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title>404 Not found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <main class="text-center pt-12">
+        <p class="text-center pb-4 font-medium text-tomato text-lg">404</p>
+        <h1 class="font-semibold text-strong text-center text-3xl sm:text-5xl tracking-tighter">
+          Page not found
+        </h1>
+      </main>
     </main>
   );
 }
